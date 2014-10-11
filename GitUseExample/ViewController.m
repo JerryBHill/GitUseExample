d8f190f7e78fa673f233fcb442092b989306038e
JerryBHill <jerry.hill@me.com>
2014-10-11 16:39:51 -0400

    Add button

--- GitUseExample/ViewController.m
+++ GitUseExample/ViewController.m
@@ -14,14 +14,11 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
+#pragma mark - IBActions
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)clickTheButton:(id)sender
+{
+    NSLog(@"This is a Git tutorial");
 }
 
 @end
