6a9b6bfd159e6947607e8c22ce39f8e7bd7dfacb
JerryBHill <jerry.hill@me.com>
2014-10-11 16:45:06 -0400

    New branch Map stuff

---
+++ GitUseExample/MapForItenerary.h
@@ -0,0 +1,13 @@
+//
+//  MapForItenerary.h
+//  GitUseExample
+//
+//  Created by Jerry Hill on 10/11/14.
+//  Copyright (c) 2014 JumpCreek Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForItenerary : NSObject
+
+@end
---
+++ GitUseExample/MapForItenerary.m
@@ -0,0 +1,13 @@
+//
+//  MapForItenerary.m
+//  GitUseExample
+//
+//  Created by Jerry Hill on 10/11/14.
+//  Copyright (c) 2014 JumpCreek Software. All rights reserved.
+//
+
+#import "MapForItenerary.h"
+
+@implementation MapForItenerary
+
+@end
